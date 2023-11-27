b4565d8542c631c81bb5a9f6ec4b6d0c86eb1d38
singha155 <singha155@conventry.ac.uk>
2023-11-27 19:18:36 +0000

    Split up Encryption to be in its own file, Server.py file created

--- Server.py
+++ Server.py
@@ -1,13 +1,26 @@
 import socket 
-
-def Encryption(string, key):
-    
-    string =[ord(char)for char in string]
-    key = [ord(char) for char in key]
-
-    encrypt = [string ^ key for string, key in zip(string, key * (len(string) // len(key) + 1))]
-    message = ''.join(chr(byte) for byte in encrypt)
-
-    return message 
-
+ 
 def Server(host, port):
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(2)
+    print("Server is running")
+
+    while True:
+        client_Socket, client_Address = server.accept()
+
+        print(f"Conneted: {client_Address}")
+        message = client_Socket.recv(1024)
+        print(f"Data recived: {message.decode('utf-8')}")
+
+        client_Socket.sendall(message)
+
+        client_Socket.close()
+        print("End of connection")
+
+if __name__ == '__main__':
+    server_Ip = '127.0.0.1'
+    server_Port = 1234
+
+    Server(server_Ip, server_Port)
