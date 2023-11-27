d932a3350a3b81ef8d885d4d4ef54e5a8c02292b
singha155 <singha155@conventry.ac.uk>
2023-11-27 18:56:22 +0000

    first commit

---
+++ Readme.txt.txt
@@ -0,0 +1,1 @@
+Here is my simple implementation, of an encrypted message system being sent though a sever to a client using python.
---
+++ Server.py
@@ -0,0 +1,13 @@
+import socket 
+
+def Encryption(string, key):
+    
+    string =[ord(char)for char in string]
+    key = [ord(char) for char in key]
+
+    encrypt = [string ^ key for string, key in zip(string, key * (len(string) // len(key) + 1))]
+    message = ''.join(chr(byte) for byte in encrypt)
+
+    return message 
+
+def Server(host, port):
